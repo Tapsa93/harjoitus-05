78a7ef215337b1570e065f9430b66d5749658608
Tapsa93 <Tapio.pek@hotmail.com>
2014-09-24 18:34:45 +0300

    Harjoitus 5
    Tapio Pekkarinen

---
+++ Harjoitus 5.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+int erotus;
+void Laske(int eka, int toka);
+void main()
+{
+	int x, y;
+	cout << "Anna 2 kokonaislukua";
+	cin >> x >> y;
+	Laske(x, y);
+	if (erotus == 0)
+		cout << "BINGO";
+	else
+		cout << "BONGO";
+	
+	
+
+
+}
+void Laske(int eka, int toka)
+{
+	erotus = eka - toka;
+}
